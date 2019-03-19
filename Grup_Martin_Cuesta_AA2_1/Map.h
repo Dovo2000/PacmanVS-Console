357079fa006642db048d70cc56a00af3c2f9c332
David Martín <david.martin@enti.cat>
2019-03-19 18:05:29 +0100

    Constructores y dos funciones aiuda

--- Grup_Martin_Cuesta_AA2_1/Map.h
+++ Grup_Martin_Cuesta_AA2_1/Map.h
@@ -1,6 +1,24 @@
 #pragma once
+#include <iostream>
+#include <Windows.h>
+
 namespace Map {
 
-	
+	struct Map
+	{
+		const int ROWS_BY_DEFAULT = 21;
+		const int COLUMNS_BY_DEFAULT = 19;
+		int rows;
+		int columns;
+		char** data;
+
+		Map();
+		Map(int rows, int columns);
+
+		bool correctMove();
+
+		void printMap();
+
+	};
 
 }
