ffa0d8e9d1133e281e2b2cac4e279bdde41b9838
David Martín <david.martin@enti.cat>
2019-03-24 12:42:14 +0100

    Creadas funciones en el player.h

--- Grup_Martin_Cuesta_AA2_1/Player.h
+++ Grup_Martin_Cuesta_AA2_1/Player.h
@@ -5,6 +5,17 @@ namespace Player {
 
 	struct Pacman {
 
+		int posX;
+		int posY;
+		int score;
+
+		Pacman();				//Constructor en el que definiremos la posicion inicial del player y inicializamos el score a 0
+
+		bool onPoint();			//Bool para saber si el jugador se encuentra en una posición con un punto o no
+		void getPoints();		//Sumará 1 al score al detectar que onPoint == true
+		void printPoints();
+		bool allowMovement();	//Comprueba si el jugador puede moverse en una dirección
+		void movePlayer();		//Usando la función de Windows.h GetAsyncKeyState(VirtualKey); VK_RIGHT, VK_LEFT, VK_UP, VK_DOWN
 
 
 
