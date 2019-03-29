ac37b815393b8d356e21c9df88e120bb7368b712
David Martín <david.martin@enti.cat>
2019-03-29 22:39:22 +0100

    c

--- Grup_Martin_Cuesta_AA2_1/Player.cpp
+++ Grup_Martin_Cuesta_AA2_1/Player.cpp
@@ -1,4 +1,13 @@
 #include "Player.h"
+#include "Map.h"
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
+
+Pacman::Pacman() {
+
+	posX = 15;
+	posY = 15;
+	score = 0;
+	Map::table[15][15] = player;
+}
